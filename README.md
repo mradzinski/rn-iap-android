11c5b557c70c5681f00a26797879a4462d567d00
Matias <mradzinski@gmail.com>
2018-05-14 10:39:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rn-iap-android
---
+++ app/src/main/java/io/mradzinki/reactnativeiap/App.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018. DNA Software. All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.mradzinki.reactnativeiap;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
